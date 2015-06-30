d376af64aec7c5f27278d72c4a8c93edc2212b82
Robert Mannuzza <Robert.mannuzza@gmail.com>
2015-06-29 22:33:02 -0400

    1111

--- src/com/cbthinkx/spaceinvaders/SpaceInvadersMain.java
+++ src/com/cbthinkx/spaceinvaders/SpaceInvadersMain.java
@@ -1,26 +1,26 @@
 package com.cbthinkx.spaceinvaders;
 
+import javax.swing.*;
 import java.awt.EventQueue;
 
-import com.cbthinkx.puzzler.menu.ThePuzzler;
 
-public class SpaceInvaders extends JFrame {
+public class SpaceInvadersMain extends JFrame {
 	private static final long serialVersionUID = 1L;
     private int height = 750;
     private int width = 750;
     private GameView panel;
-	public SpaceInvaders() {
+	public SpaceInvadersMain() {
 	 	setDefaultCloseOperation(EXIT_ON_CLOSE);
     	setSize(width, height);
     	setResizable(false);
     	setLocationRelativeTo(null);
 		setVisible(true);
 		panel = new GameView();
-		add(panel);
+//		add(panel);
 	}
 	public static void main(String[] sa) {
 		EventQueue.invokeLater(
-				() -> new SpaceInvaders()
+				() -> new SpaceInvadersMain()
 		);
 	};
 
