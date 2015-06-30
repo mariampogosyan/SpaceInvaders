9ee49ad182a732e9ff145ce5ba350ea1ef533fa5
Mariam Pogosyan <vmariam.pogosyan@gmail.com>
2015-06-29 23:04:56 -0400

    jframe yay

--- src/com/cbthinkx/spaceinvaders/GameView.java
+++ src/com/cbthinkx/spaceinvaders/GameView.java
@@ -1,5 +1,13 @@
 package com.cbthinkx.spaceinvaders;
 
-public class GameView {
+import java.awt.Color;
+
+import javax.swing.JPanel;
+
+public class GameView extends JPanel {
+	private static final long serialVersionUID = 1L;
+	public GameView() {
+		setBackground(Color.BLACK);
+	}
 
 }
--- src/com/cbthinkx/spaceinvaders/SpaceInvaders.java
+++ src/com/cbthinkx/spaceinvaders/SpaceInvaders.java
@@ -1,26 +1,29 @@
 package com.cbthinkx.spaceinvaders;
 
+
 import javax.swing.*;
+
 import java.awt.EventQueue;
 
 
-public class SpaceInvadersMain extends JFrame {
+public class SpaceInvaders extends JFrame {
 	private static final long serialVersionUID = 1L;
     private int height = 750;
     private int width = 750;
     private GameView panel;
-	public SpaceInvadersMain() {
+	public SpaceInvaders() {
+		super("Space Invaders");
 	 	setDefaultCloseOperation(EXIT_ON_CLOSE);
     	setSize(width, height);
     	setResizable(false);
     	setLocationRelativeTo(null);
 		setVisible(true);
 		panel = new GameView();
-//		add(panel);
+		add(panel);
 	}
 	public static void main(String[] sa) {
 		EventQueue.invokeLater(
-				() -> new SpaceInvadersMain()
+				() -> new SpaceInvaders()
 		);
 	};
 
