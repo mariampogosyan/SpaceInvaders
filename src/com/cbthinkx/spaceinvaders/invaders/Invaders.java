8f6a856a60b6f1ac8ce362e9559847dbd9c825e6
Mariam Pogosyan <vmariam.pogosyan@gmail.com>
2015-07-27 19:52:53 -0400

    variables

--- src/com/cbthinkx/spaceinvaders/invaders/Invaders.java
+++ src/com/cbthinkx/spaceinvaders/invaders/Invaders.java
@@ -1,5 +1,28 @@
 package com.cbthinkx.spaceinvaders.invaders;
 
-public class Invaders {
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
 
+import com.sun.javafx.geom.Path2D;
+
+public class Invaders {
+	private int score;
+	private int hitPoints;
+	private boolean isAlive;
+	private boolean canShoot;
+	private int x;
+	private int y;
+	private int row;
+	private ArrayList<BufferedImage> img;
+	private Path2D hitbox;
+	public void updatePosition() {
+		
+	}
+	public void blowUp() {
+		
+	}
+	public void UpdateImage() {
+		
+	}
+	
 }
--- src/com/cbthinkx/spaceinvaders/missiles/Missiles.java
+++ src/com/cbthinkx/spaceinvaders/missiles/Missiles.java
@@ -1,5 +1,26 @@
 package com.cbthinkx.spaceinvaders.missiles;
 
-public class Missiles {
+import java.awt.geom.Path2D;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
 
+public class Missiles {
+	private float speed;
+	private boolean isAlive;
+	private boolean isDestroyable;
+	private int x;
+	private int y;
+	private Path2D hitbox;
+	private ArrayList<BufferedImage> img;
+	public void updatePosition() {
+		
+	}
+	public void blowUp()
+	{
+		
+	}
+	public void UpdateImage() {
+		
+	}
+	
 }
